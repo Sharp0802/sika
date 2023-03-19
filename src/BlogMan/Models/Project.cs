ef48079a3a10e4cbf18982b2c6e11568f9f9b664
Sharp0802 <sharp0802.business@gmail.com>
2023-03-19 14:54:32 +0900

    Create a project model
    - src/BlogMan/Models/Project.cs: Create a project model

---
+++ src/BlogMan/Models/Project.cs
@@ -0,0 +1,22 @@
+using System.Xml.Serialization;
+
+namespace BlogMan.Models;
+
+[Serializable, XmlRoot]
+public class Project
+{
+    public string Name { get; set; }
+    public Version ApiVersion { get; set; }
+    public string PostDirectory { get; set; }
+    public string SiteDirectory { get; set; }
+    public string BuildDirectory { get; set; }
+
+    public Project(string name, Version api, string post, string site, string build)
+    {
+        Name = name;
+        ApiVersion = api;
+        PostDirectory = post;
+        SiteDirectory = site;
+        BuildDirectory = build;
+    }
+}
