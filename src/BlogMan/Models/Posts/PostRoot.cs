2e4295140d117847f2ca30f30ddac76d91f54c74
Sharp0802 <sharp0802.business@gmail.com>
2023-10-05 18:18:16 +0900

    Fix #18: Sort file-tree
    - src/BlogMan/Models/Posts/PostBranch.cs:
        - Expose directory info
        - Sort children when converting into html
    - src/BlogMan/Models/Posts/PostRoot.cs: Sort children when converting into html
    - src/BlogMan/Models/Posts/PostTreeComparer.cs: Implement comparer for PostTree

--- src/BlogMan/Models/Posts/PostRoot.cs
+++ src/BlogMan/Models/Posts/PostRoot.cs
@@ -19,13 +19,14 @@ public class PostRoot : PostRootImpl
     public PostRoot(FileSystemInfo? parent, FileSystemInfo current) : base(parent, current)
     {
     }
-    
+
     public new string GetHtml()
     {
         return Children
-                   ?.Select(child => child.GetHtml())
-                   .Aggregate(new StringBuilder(), (builder, html) => builder.AppendLine(html?.ToString()))
-                   .ToString()
+             ?.Order(PostTreeComparer.Default)
+              .Select(child => child.GetHtml())
+              .Aggregate(new StringBuilder(), (builder, html) => builder.AppendLine(html?.ToString()))
+              .ToString()
                ?? string.Empty;
     }
 }
