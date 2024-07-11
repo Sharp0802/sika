d3583ca3b3ad235030b30f05bf1d0a36373250fe
Sharp0802 <sharp0802.business@gmail.com>
2024-07-11 22:23:26 +0900

    :sparkles: #77: Create google sitemap linker

---
+++ src/sika.core/Components/GoogleSitemapLinker.cs
@@ -0,0 +1,47 @@
+// Copyright (C)  2024  Yeong-won Seo
+// 
+// SIKA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// SIKA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with SIKA.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Xml.Linq;
+using sika.core.Components.Abstract;
+using sika.core.Model;
+
+namespace sika.core.Components;
+
+public class GoogleSitemapLinker(Project project) : ILinker, IDisposable
+{
+    private readonly List<XObject> _urls = [];
+    private readonly XNamespace    _ns   = "http://www.sitemaps.org/schemas/sitemap/0.9";
+
+    public void Dispose()
+    {
+        var urlSet = new XElement(_ns + "urlset", _urls);
+        File.WriteAllText(
+            Path.Combine(project.Info.SiteDirectory, "sitemap.xml"), 
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + urlSet.ToString(SaveOptions.OmitDuplicateNamespaces));
+    }
+
+    public Task CompileAsync(PageTree tree)
+    {
+        var loc     = project.Info.RootUri + tree.GetFullPath();
+        var lastMod = ((PageLeafData)tree.Content).Metadata.Timestamps.Last();
+
+        var element = new XElement(_ns + "url",
+            new XElement(_ns + "loc", new XText(loc)),
+            new XElement(_ns + "lastmod", lastMod.ToString("yyyy-MM-dd")));
+        _urls.Add(element);
+
+        return Task.CompletedTask;
+    }
+}
